8ed1b4d794ed33dfd7b7dc4db415bad9ac223b9d
meltsplit <seokwoo2000@naver.com>
2022-06-26 22:49:44 +0900

    [Add] : 길건너친구들 프로젝트 좌표뷰 생성

--- Crossy Road/gameViewController.swift
+++ Crossy Road/gameViewController.swift
@@ -7,8 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class gameViewController: UIViewController {
+    
+    @IBOutlet weak var player: UIView!
+    @IBOutlet weak var car1: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
